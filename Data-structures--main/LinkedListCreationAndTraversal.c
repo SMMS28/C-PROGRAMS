44a197f3976188667d21cefa67922362391ff932
MOHAN MUKUND SAI SUNKARA <116540844+SMMS28@users.noreply.github.com>
2023-11-30 21:33:40 +0530

    Update LinkedListCreationAndTraversal.c

--- Data-structures--main/LinkedListCreationAndTraversal.c
+++ Data-structures--main/LinkedListCreationAndTraversal.c
@@ -23,7 +23,6 @@ int main()
     head=(struct Node *)malloc(sizeof(struct Node));
     second=(struct Node *)malloc(sizeof(struct Node));
     third=(struct Node*)malloc(sizeof(struct Node));
-
     head->data=10;
     head->next=second;
 
